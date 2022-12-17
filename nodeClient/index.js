952bbaee6b1013e6243744d8b0b399b42885ea79
Ibukun-tech <oyetunjiibukunoluwa8@gmail.com>
2022-12-17 14:07:55 +0100

    working on more message

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -1,23 +1,75 @@
 const os = require("os");
+const { resolve } = require("path");
 const { uptime } = require("process");
 // Required to get the cpu details
-const cpu = os.cpus();
-// operating system type
-const osType = os.type();
-console.log(osType);
-// uptime
-const upTime = os.uptime();
-console.log(upTime);
-// free memory
-const freeMemory = os.freemem();
-// total memory
-const totalMemory = os.totalmem();
-// current Memory usage
-const usedMemory = freeMemory - totalMemory;
+function cpuPerformance() {
+  return new Promise(async (resolve, reject) => {
+    const cpu = os.cpus();
+    // operating system type
+    const osType = os.type();
+    console.log(osType);
+    // uptime
+    const upTime = os.uptime();
+    console.log(upTime);
+    // free memory
+    const freeMemory = os.freemem();
+    // total memory
+    const totalMemory = os.totalmem();
+    // current Memory usage
+    const usedMemory = freeMemory - totalMemory;
 
-const memoryUsage = Math.floor((usedMemory / totalMemory) * 100) / 100;
+    const memoryUsage = Math.floor((usedMemory / totalMemory) * 100) / 100;
 
-// cpu type
-const cpuModel = cpu[0].model;
-// cpu speed
-const cpuSpeed = cpu[0].speed;
+    // cpu type
+    const cpuModel = cpu[0].model;
+    // cpu speed
+    const cpuSpeed = cpu[0].speed;
+    const cpuLoad = await getCpuLoad();
+    resolve({
+      cpu,
+      osType,
+      upTime,
+      freeMemory,
+      totalMemory,
+      usedMemory,
+      memoryUsage,
+      cpuModel,
+      cpuSpeed,
+      cpuLoad,
+    });
+  });
+}
+// we need the average od the cores which will give us the cpu average
+function cpuAverage() {
+  const cpu = os.cpus();
+  let idleMs = 0;
+  let totalMs = 0;
+  cpu.forEach((core) => {
+    for (val in core.times) {
+      totalMs += core.times[val];
+    }
+    idleMs += core.times.idle;
+  });
+  return {
+    idle: idleMs / cpu.length,
+    total: totalMs / cpu.length,
+  };
+}
+console.log(cpuAverage());
+function getCpuLoad() {
+  return new Promise((resolve, reject) => {
+    const start = cpuAverage();
+    setTimeout(() => {
+      const end = cpuAverage();
+      const idleDifference = end.idle - start.idle;
+      const totalDifference = end.total - start.total;
+      console.log(idleDifference, totalDifference);
+      const percentageCpu =
+        100 - Math.floor(100 * (idleDifference / totalDifference));
+      resolve(percentageCpu);
+    }, 100);
+  });
+}
+cpuPerformance().then((data) => {
+  console.log(data);
+});
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cluster = require("cluster");
+const net = require("net");
+const socketIo = require("socket.io");
+
+const port = 2000;
+const processes = require("os").cpus().length;
+
+const io_redis = require("socket.io-redis");
+const farmHash = require("farmhash");
+console.log(cluster);
+if (cluster.isMaster) {
+  let workers = [];
+  let spawn = function (i) {
+    workers[i] = cluster.fork();
+    workers[i].on("exit", function (code, signal) {
+      spawn(i);
+    });
+  };
+  for (let i = 0; i < processes; i++) {
+    spawn(i);
+  }
+  const worker_index = function (ip, len) {
+    return farmHash.fingerprint32(ip) % len;
+  };
+  const server = net.createServer({ pauseOnConnect: true }, (connection) => {
+    let worker = workers[worker_index(connection.remoteAddress, processes)];
+    worker.send("sticky-session:connection", connection);
+  });
+  server.listen(port);
+  console.log(`master listening on ${port}`);
+} else {
+  let app = express();
+  const server = app.listen(0, "localhost");
+  const io = socketIo(server);
+}
